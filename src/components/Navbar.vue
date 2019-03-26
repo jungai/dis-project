2c4998bdf1c49850b098994255c8469c0cd020c0
jungai <juworaphol125@gmail.com>
2019-03-26 14:49:18 +0700

    add MainLayout & Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- @comment -->
+    <h1>Nav code here</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  data: () => ({
+
+  })
+}
+</script>
+
+<style>
+
+</style>
