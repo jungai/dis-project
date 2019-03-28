23db11d6be06ebba623a4252a9a97a785441f6d9
jungai <juworaphol125@gmail.com>
2019-03-28 11:33:24 +0700

    new version

--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -1,19 +1,33 @@
 <template>
   <div>
-    <Navbar/>
+    <Navbar
+      :itClick="clickSignIn"
+      @did-click="clickSignIn = $event"/>
+      <!-- @view should be here -->
+    <transition
+      name="fade">
+        <ModalLogin
+          v-if="clickSignIn"/>
+    </transition>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
+import ModalLogin from '@/components/ModalLogin'
 
 export default {
   components: {
-    Navbar
-  }
+    Navbar,
+    ModalLogin
+  },
+  data: () => ({
+    clickSignIn: false
+  })
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@import '@/styles/mixins/fade.scss';
+@include fade();
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,35 @@
 <template>
   <div>
-    <!-- @comment -->
-    <h1>Nav code here</h1>
+    <b-navbar class="p-1 border border-secondary">
+      <b-navbar-brand href="#">
+        <img src="@/assets/mike.png" alt="wait for real" class="logo">
+      </b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item
+          @click="$emit('did-click', !itClick)">
+          <span class="h2">
+            <i class="far fa-user-circle"></i>
+          </span>
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
   </div>
 </template>
 
 <script>
 export default {
   name: 'navbar',
-  data: () => ({
-
-  })
+  props: {
+    itClick: {
+      type: Boolean,
+      default: false
+    }
+  }
 }
 </script>
 
 <style>
-
+.logo {
+  height: 38px;
+}
 </style>
