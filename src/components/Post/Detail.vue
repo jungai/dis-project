36873bd1ed32656a30c32b0a295d01cf642420e3
jungai <juworaphol125@gmail.com>
2019-04-11 12:42:56 +0700

    add post components

---
+++ src/components/Post/Detail.vue
@@ -0,0 +1,18 @@
+<template>
+  <p> {{reviews }} </p>
+</template>
+
+<script>
+export default {
+  props: {
+    reviews: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/PostDetail.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Title :text="data.title"/>
+    <Detail :reviews="data.review"/>
+  </div>
+</template>
+
+<script>
+import Title from '@/components/Post/Title'
+import Detail from '@/components/Post/Detail'
+import axios from 'axios'
+
+export default {
+  components: {
+    Title,
+    Detail
+  },
+  async created () {
+    try {
+      const { data } = await axios.get('http://localhost:3000/reviews')
+      console.log(data[0])
+      this.data = data[0]
+    } catch (error) {
+    }
+  },
+  data: () => ({
+    data: []
+  })
+}
+</script>
+
+<style>
+
+</style>
