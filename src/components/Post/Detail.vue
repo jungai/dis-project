ffa012d956be02a28f7067005c7332855bdf7806
jungai <juworaphol125@gmail.com>
2019-04-25 11:47:09 +0700

    use social-shared

--- src/components/Post/Detail.vue
+++ src/components/Post/Detail.vue
@@ -1,5 +1,17 @@
 <template>
-  <p> {{ review }} </p>
+  <div>
+    <p class="mb-4"> {{ review }} </p>
+    <social-sharing url="https://vuejs.org/" inline-template>
+      <div class="d-flex justify-content-center">
+        <network network="facebook" class="m-2">
+         <i class="fab fa-facebook img" id="facebook"/>
+        </network>
+        <network network="twitter" class="m-2">
+          <i class="fab fa-twitter-square img" id="twitter"/>
+        </network>
+      </div>
+    </social-sharing>
+  </div>
 </template>
 
 <script>
@@ -14,5 +26,18 @@ export default {
 </script>
 
 <style>
-
+.img {
+  font-size: 2.8em;
+}
+#twitter {
+  color: #3498db;
+}
+#facebook {
+  color: #3C5A99
+}
+@media (max-width: 768px) {
+  .img {
+    font-size: 2.2em;
+  }
+}
 </style>
